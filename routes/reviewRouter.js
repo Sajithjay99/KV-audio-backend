7da5b7cd3ccb15c719cab2e03324f394f2d69acf
Sajithjay99 <sajithjay99@gmail.com>
2025-01-16 13:06:23 +0530

    add getReviews endpoint to retrieve reviews based on user role

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {addReview} from '../controller/reviewController.js';
+import {getReviews} from '../controller/reviewController.js';
 
 
 const reviewRouter = express.Router();
 
 reviewRouter.post('/add',addReview)
+reviewRouter.get('/get',getReviews)
 
 export default reviewRouter;
